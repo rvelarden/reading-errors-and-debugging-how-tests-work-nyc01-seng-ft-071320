dc497404630a4120693a34e1d7c70bc9ba8fd07e
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-09 21:23:44 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
 # Add your variables here
+first_number = 1.1 
+puts first_number
+
+second_number = 1.2 
+puts second_number
+
+sum = first_number + second_number
+puts sum
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
